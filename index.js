74a2146fcb4e8abfb3e87f1b88d52d57e35b11ba
ficerman <ficermanlaura@gmail.com>
2021-11-21 02:45:04 +0100

    .

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+let backgrounds = [
+	'linear-gradient(-45deg, #6fa3f1, #ec8fb3, #eca7ca, #acebc8)',
+	'linear-gradient(-45deg, #6fa3f1, #8fece7, #68cc81, #c3b1f5)',
+]
+
 function winOpenFicerman(zad) {
 	window.open(
 		'zad' + zad + '.html',
-		'okienko' + zad,
+		'okienko',
 		'toolbar=no,directories=no,menubar=no,height=1000,width=1000,top=200,left=200'
 	)
 }
@@ -12,15 +17,37 @@ function dodajZadFicerman(zad) {
 	zadP.className = 'zadP'
 	zadP.setAttribute('onclick', 'winOpenFicerman(' + zad + ')')
 	zadP.setAttribute('id', zad)
-	let zadPKod = document.createElement('button')
+	/*let zadPKod = document.createElement('button')
 	zadPKod.innerHTML = 'kod'
 	zadPKod.className = 'zadP kod'
-	zadP.appendChild(zadPKod)
+	zadP.appendChild(zadPKod)*/
 	document.getElementById('mainContainer').appendChild(zadP)
 }
 
+function kodOpenFicerman(zad) {
+	window.open(
+		'zad' + zad + 'kod.html',
+		'okienko',
+		'toolbar=no,directories=no,menubar=no,height=1000,width=1000,top=200,left=200'
+	)
+}
+
+function dodajKodFicerman(zad) {
+	let zadP = document.createElement('button')
+	zadP.innerHTML = 'zad. ' + zad + ' kod'
+	zadP.className = 'zadP'
+	zadP.setAttribute('onclick', 'kodOpenFicerman(' + zad + ')')
+	zadP.setAttribute('id', zad + 'kod')
+	/*let zadPKod = document.createElement('button')
+	zadPKod.innerHTML = 'kod'
+	zadPKod.className = 'zadP kod'
+	zadP.appendChild(zadPKod)*/
+	document.getElementById('mainContainer').appendChild(zadP)
+}
 
 document.addEventListener('DOMContentLoaded', function () {
+	kodyOn = false
+	document.getElementById('mainContainer').replaceChildren()
 	for (let k = 0; k < 29; k++) {
 		dodajZadFicerman(k + 1)
 	}
@@ -31,11 +58,16 @@ var amongusDrip = new Audio('amongusdriptheme.mp3')
 var killSound = new Audio('killsound.mp3')
 let kotek = document.getElementById('kotek')
 kotek.addEventListener('click', function () {
-	kotekPermaMad = true
-	bodyRep.pause()
-	killSound.play()
-	amongusDrip.currentTime = bodyRep.currentTime
-	amongusDrip.play()
+	if (kotekPermaMad == false) {
+		kotekPermaMad = true
+		bodyRep.pause()
+		killSound.play()
+		amongusDrip.currentTime = bodyRep.currentTime
+		amongusDrip.play()
+		document.getElementById('cat1').style.opacity = '100'
+		document.getElementById('cat2').style.opacity = '100'
+		document.getElementById('cat3').style.opacity = '100'
+	}
 })
 kotek.addEventListener('mouseenter', function () {
 	if (kotekPermaMad == false) {
@@ -43,11 +75,108 @@ kotek.addEventListener('mouseenter', function () {
 		bodyRep.reset
 		bodyRep.currentTime = 0
 		bodyRep.play()
+		document.getElementById('cat1').style.display = 'block'
+		document.getElementById('cat2').style.display = 'block'
+		document.getElementById('cat3').style.display = 'block'
 	}
 })
 kotek.addEventListener('mouseleave', function () {
 	if (kotekPermaMad == false) {
 		kotek.setAttribute('src', 'icon.ico')
 		bodyRep.pause()
+		document.getElementById('cat1').style.display = 'none'
+		document.getElementById('cat2').style.display = 'none'
+		document.getElementById('cat3').style.display = 'none'
+	}
+})
+amongusDrip.addEventListener(
+	'ended',
+	function () {
+		document.getElementById('cat1').style.opacity = '0'
+		document.getElementById('cat2').style.opacity = '0'
+		document.getElementById('cat3').style.opacity = '0'
+		document
+			.getElementsByClassName('cat')
+			.addEventListener(this.getAttribute.opacity == 0)
+		{
+			this.style.display = 'none'
+		}
+	},
+	false
+)
+
+let chemiaNowejEry = new Audio('muzyka z chemii nowej ery (vaporwave edit).mp3')
+scriptExtended = new Boolean(false)
+
+document.getElementById('scrollArrow').addEventListener('click', function () {
+	document
+		.getElementById('breakingBadScript')
+		.setAttribute('style', 'height: 10000vh; overflow: hidden;')
+	this.style.opacity = '0'
+	scriptExtended = true
+	document.body.style.backgroundSize = '10000vh'
+	bodyRep.pause()
+	amongusDrip.pause()
+	amongusDrip.volume = 0
+	bodyRep.volume = 0
+	kotekPermaMad = true
+	chemiaNowejEry.play()
+	document.getElementById('mainContainer').style.opacity = 0
+	document.getElementById('nazwisko').style.opacity = 0
+	document.getElementById('kotek').style.opacity = 0
+	document.getElementById('settings').style.opacity = 0
+	document.getElementById('footer').style.opacity = 0
+	document.getElementById('breakingBadHeader').style.opacity = 100
+})
+
+document
+	.getElementById('scrollArrow')
+	.addEventListener('mouseenter', function () {
+		document.getElementById('breakingBadHeader').style.display = 'block'
+	})
+
+document
+	.getElementById('scrollArrow')
+	.addEventListener('mouseleave', function () {
+		if ((scriptExtended = false)) {
+			document.getElementById('breakingBadHeader').style.display = 'none'
+		}
+	})
+
+chemiaNowejEry.addEventListener(
+	'ended',
+	function () {
+		this.currentTime = 0
+		this.play()
+	},
+	false
+)
+
+let settings = document.getElementById('settings')
+kodyOn = new Boolean(false)
+
+settings.addEventListener('click', function () {
+	if (kodyOn == false) {
+		document.body.style.background =
+			'linear-gradient(-45deg, #98abc7, #8fece7, #68ccbb, #c3b1f5)'
+		document.body.style.backgroundSize = '400vw 400vh'
+		document.body.style.animation = 'gradient 15s ease infinite'
+
+		document.getElementById('mainContainer').replaceChildren()
+		for (let k = 0; k < 29; k++) {
+			dodajKodFicerman(k + 1)
+		}
+		kodyOn = true
+	} else if (kodyOn == true) {
+		kodyOn = false
+		document.body.style.background =
+			'linear-gradient(-45deg, #6fa3f1, #ec8fb3, #eca7ca, #acebc8)'
+		document.body.style.backgroundSize = '400vw 400vh'
+		document.body.style.animation = 'gradient 15s ease infinite'
+
+		document.getElementById('mainContainer').replaceChildren()
+		for (let k = 0; k < 29; k++) {
+			dodajZadFicerman(k + 1)
+		}
 	}
 })
